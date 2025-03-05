01d90d5d3a23686fb3220fcf8f6e7983fa2a8624
ritvik96 <ritvikmittal96@gmail.com>
2025-03-05 09:52:41 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket("https://chat-room-se17.onrender.com");
+const socket = new WebSocket("https://chat-room-tezr.onrender.com");
 
 const chatContainer = document.getElementById("chat-container");
 const chatBox = document.getElementById("chat-box");
